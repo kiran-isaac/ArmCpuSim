b43f94776b42126e881a54f55d27c219086f83ca
Kiran <kiranisturt@outlook.com>
2025-01-25 16:40:15 +0000

    updated log formatting

--- src/log.rs
+++ src/log.rs
@@ -2,32 +2,44 @@ use std::io::Write;
 
 use crate::{model::Registers, I, IT::*};
 
-
 pub struct Logger {
     file: std::fs::File,
     previous_state: Registers,
 }
 
 impl Logger {
-    pub fn new(filename : &str, initialstate: &Registers) -> Logger {
+    pub fn new(filename: &str, initialstate: &Registers) -> Logger {
         let mut file = std::fs::File::create(filename).unwrap();
-        file.write(b"Instruction,IT,Rd,Rn,Rm,Rt,Rl,Immu,Imms\n").unwrap();
-        Logger { file, previous_state: initialstate.clone() }
+        file.write(b"Instruction   ,Rd   ,Rn   ,Rm   ,Rt   ,Rl                 ,Immu     ,Imms\n")
+            .unwrap();
+        Logger {
+            file,
+            previous_state: initialstate.clone(),
+        }
     }
 
     fn reg_id_to_str(id: u8) -> String {
         match id {
-            0..=12 => format!("r{}", id),
-            13 => "sp".to_string(),
-            14 => "lr".to_string(),
-            15 => "pc".to_string(),
+            0..=12 => format!("{}", id),
+            13 => "SP".to_string(),
+            14 => "LR".to_string(),
+            15 => "PC".to_string(),
             _ => panic!("Invalid register index"),
         }
     }
 
     // Will log in order: IT, Rd, Rn, Rm, Rt, Rl, Immu, Imms
     fn instr_args_log(i: I) -> String {
-        format!("{:?}, {}, {}, {}, {}, {:#018b}, {}, {}", i.it, i.rd, i.rn, i.rm, i.rt, i.rl, i.immu, i.imms)
+        format!(
+            "{:<2}   ,{:<2}   ,{:<2}   ,{:<2}   ,{:018b} ,{:#08X} ,{:#08X}",
+            Self::reg_id_to_str(i.rd),
+            Self::reg_id_to_str(i.rn),
+            Self::reg_id_to_str(i.rm),
+            Self::reg_id_to_str(i.rt),
+            i.rl,
+            i.immu,
+            i.imms
+        )
         // match i.it {
         //     // Rd, Rn, Rm
         //     ADDReg => {
@@ -59,7 +71,16 @@ impl Logger {
     }
 
     pub fn log(&mut self, i: I, state: &Registers) {
-        self.file.write(format!("{:?},{}\n", i.it, Self::instr_args_log(i)).as_bytes()).unwrap();
+        self.file
+            .write(
+                format!(
+                    "{:<11}   ,{}\n",
+                    format!("{:?}", i.it),
+                    Self::instr_args_log(i)
+                )
+                .as_bytes(),
+            )
+            .unwrap();
 
         // self.previous_state = state.clone()
     }
