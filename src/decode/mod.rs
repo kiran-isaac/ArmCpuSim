0887eab4d1300b35143aa10447fb59b9fc2e5f3e
Kiran <kiranisturt@outlook.com>
2025-03-16 22:50:02 +0000

    get issue type

--- src/decode/mod.rs
+++ src/decode/mod.rs
@@ -3,3 +3,38 @@ mod decode2;
 
 pub use decode1::*;
 pub use decode2::*;
+
+pub enum IssueType {
+    ALU,
+    Shift,
+    MUL,
+    Load,
+    Store,
+    /// Writing to PC, branching, system calls
+    Control,
+}
+
+use IT::*;
+
+pub fn get_issue_type(it: IT) -> IssueType {
+    match it {
+        ADC | ADDImm | ADDReg | ADDSpImm | ADR | AND | BIC
+        | CMN | CMPImm | CMPReg | EOR | MOVImm | MOVReg | MVN
+        | ORR | REVSH | REV16 | REV | RSB | SBC | ROR | SUBImm
+        | SUBSP | SUBReg | SXTB | SXTH | UXTB | UXTH | TST => IssueType::ALU,
+
+        MUL => IssueType::MUL,
+
+        B | LoadPc | BL | BLX | BX | SVC => IssueType::Control,
+
+        STRImm | STRReg | STRBImm | STRBReg | STRHImm | STRHReg=> IssueType::Store,
+
+        LDRImm | LDRLit | LDRReg | LDRHImm | LDRHReg | LDRBImm | LDRBReg | LDRSB | LDRSH => IssueType::Load,
+
+        ASRImm | ASRReg | LSLImm | LSRImm | LSRReg | LSLReg => IssueType::Shift,
+
+        // fails when hits these instructions, cannot issue
+        NOP | DSB | ISB | DMB | BKPT | MRS | MSR | UNDEFINED | UNPREDICTABLE | SEV | WFE | WFI | YIELD  => panic!("Got instruction that shouldn't be in an application level binary, system level instructions not supported"),
+        LDMIA | STMIA | POP | PUSH => panic!("Got ciscy instruction {:?} in issue, should have been broken down", it),
+    }
+}
